0e95c43afebcf94239e86edf2e62bb21a48cda04
Yuma Ishikawa <yuma.fuu05@gmail.com>
2023-02-12 01:09:08 +0900

    [fix] compile

--- cli/cmds/add.ts
+++ cli/cmds/add.ts
@@ -6,59 +6,62 @@ import {
 } from "https://deno.land/x/cliffy@v0.25.7/prompt/mod.ts";
 import axiod from "https://deno.land/x/axiod/mod.ts";
 import { download } from "https://deno.land/x/download/mod.ts";
-
 import $ from "https://deno.land/x/dax/mod.ts";
 
 export const Add = new Command()
   .description("Initialize Starry Repo")
   .action(async () => {
-    await main()
+    await add();
   });
 
-async function main() {
-  const apiUrl = "https://api.github.com/repos/ispec-inc/starry/releases/latest"
-  const response = await axiod.get(apiUrl)
+type Asset = {
+  name: string;
+  browser_download_url: string;
+};
+
+async function add() {
+  const apiUrl =
+    "https://api.github.com/repos/ispec-inc/starry/releases/latest";
+  const response = await axiod.get(apiUrl);
   const data = response.data.assets.filter((f: any) => {
-    return f.content_type === "application/zip"
-  })
+    return f.content_type === "application/zip";
+  });
 
-  const names = data.map((asset: any) => {
-    return asset.name.replace(".zip", "")
-  })
+  const names: string[] = data.map((asset: Asset) => {
+    return asset.name?.replace(".zip", "");
+  });
 
-  let dirs = {}
-  data.forEach((asset: any) => {
-    const name = asset.name.replace(".zip", "")
-    dirs[name] = asset.browser_download_url
-  })
+  let dirs: any = {};
+  data.forEach((asset: Asset) => {
+    const name = asset.name?.replace(".zip", "");
+    dirs[name] = asset.browser_download_url as string;
+  });
 
   const result = await Checkbox.prompt({
-    name: "dirs",
     message: "Select Directory",
-    type: Checkbox,
     options: names,
   });
 
   const confirmed = await Confirm.prompt({
     message: "Are you sure to initialize these packages?",
     hint: `${result.join(", ")}`,
-  })
+  });
 
   if (!confirmed) {
-    console.log("exit")
-    Deno.exit()
+    console.log("exit");
+    Deno.exit();
   } else {
-    console.log()
-    console.log(colors.bold.blue("Affirmative"))
+    console.log();
+    console.log(colors.bold.blue("Affirmative"));
   }
 
   for (const dir of result) {
-    const url = dirs[dir]
-    const file = `${dir}.zip`
-    await download(url, { file, dir: "." })
-    const ok = await $`unzip -uq ${file} && rm ${file}`
+    const url = dirs[dir] as string;
+    const file = `${dir}.zip`;
+    await download(url, { file, dir: "." });
+    const ok = await $`unzip -uq ${file} && rm ${file}`;
     if (ok) {
-      console.log(`${colors.bold.blue("Created")}: ${dir}`)
+      console.log(`${colors.bold.blue("Created")}: ${dir}`);
     }
   }
 }
