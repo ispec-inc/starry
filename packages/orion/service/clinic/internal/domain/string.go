a2862410dd03eac1e39879b460a55fd7a9f5cd8e
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2023-07-12 10:00:23 +0900

    doc: add comment

--- packages/orion/service/clinic/internal/domain/string.go
+++ packages/orion/service/clinic/internal/domain/string.go
@@ -9,6 +9,7 @@ import (
 // 文字数のバリデーションを共通化する
 type String string
 
+// ValidateLength 文字数のバリデーション
 func (s String) ValidateLength(min, max int) error {
 	length := utf8.RuneCountInString(string(s))
 	if length < min || length > max {
@@ -18,6 +19,7 @@ func (s String) ValidateLength(min, max int) error {
 	return nil
 }
 
+// String String型の文字列を返す
 func (s String) String() string {
 	return string(s)
 }
