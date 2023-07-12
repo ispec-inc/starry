83db70011e996871f596b9b139e07f1de9dec998
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2023-07-12 10:03:53 +0900

    doc: add doc comment

--- packages/orion/service/clinic/internal/domain/error.go
+++ packages/orion/service/clinic/internal/domain/error.go
@@ -3,6 +3,6 @@ package domain
 import "errors"
 
 var (
-	ErrUnauthorized        = errors.New("unauthorized") // ErrUnauthorized 認証に失敗したエラー
-	ErrStringInvalidLength = errors.New("string: invalid length")
+	ErrUnauthorized        = errors.New("unauthorized")          // ErrUnauthorized 認証に失敗したエラー
+	ErrStringInvalidLength = errors.New("invalid string length") // ErrStringInvalidLength 文字列の長さが不正なエラー
 )
--- packages/orion/service/clinic/internal/domain/organization/error.go
+++ packages/orion/service/clinic/internal/domain/organization/error.go
@@ -3,5 +3,5 @@ package organization
 import "errors"
 
 var (
-	ErrPhoneNumberInvalidFormat = errors.New("phone_number: invalid format")
+	ErrPhoneNumberInvalidFormat = errors.New("phone_number: invalid format") // ErrPhoneNumberInvalidFormat 電話番号のフォーマットが不正
 )
