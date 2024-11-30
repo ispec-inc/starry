c7341c2f8cfe1ddb149a71d85f78d160f6154fab
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2024-11-30 11:54:02 +0900

    feat: Implement GraphQL module with routing and schema configuration

--- packages/server-side-kotlin/src/main/kotlin/com/sdp/Application.kt
+++ packages/server-side-kotlin/src/main/kotlin/com/sdp/Application.kt
@@ -1,17 +1,32 @@
 package com.sdp
 
+import com.expediagroup.graphql.server.ktor.GraphQL
+import com.expediagroup.graphql.server.ktor.graphQLPostRoute
+import com.expediagroup.graphql.server.ktor.graphiQLRoute
 import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main(args: Array<String>) {
   io.ktor.server.netty.EngineMain.main(args)
 }
 
-fun Application.module() {
+fun Application.graphQLModule() {
   configureSecurity()
   configureSerialization()
   configureDatabases()
   configureFrameworks()
   configureHTTP()
   configureRouting()
-  configureGraphQL()
+  install(GraphQL) {
+    schema {
+      packages = listOf("com.sdp")
+      queries = listOf(HelloWorldQuery())
+    }
+  }
+  routing {
+    graphQLPostRoute()
+    graphiQLRoute()
+  }
 }
